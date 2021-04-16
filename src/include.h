6840fd3bddcf4166ada441db0bdda1218425bf2b
ACBob <acbobcat@protonmail.com>
2021-04-16 21:09:58 +0100

    My first vector class

--- src/include.h
+++ src/include.h
@@ -1,5 +1,89 @@
 // TODO: BETTER NAME
 // This should be globally included by all files
 
+#pragma once
+
+#include "box2d/b2_math.h" // b2Vec2
+#include "raylib.h" // Vector2
+
 #include <string>
 using str = std::string;
+
+// Vector that should act as an intermediary between all the other vectors the global namespaces are infected with
+class Vector
+{
+	public:
+		float x, y, z = 0;
+
+		Vector(float x, float y, float z=0)
+		{
+			this->x, this->y, this->z = x, y, z;
+		}
+	// conversion, from
+		Vector(Vector2 raylibVec)
+		{
+			x, y = raylibVec.x, raylibVec.y;
+		}
+		Vector(b2Vec2 box2dVec)
+		{
+			x, y = box2dVec.x, box2dVec.y;
+		}
+	// conversion, to
+		operator Vector2()
+		{
+			return Vector2({x,y});
+		}
+		operator b2Vec2()
+		{
+			return b2Vec2(x,y);
+		}
+
+	// Operators
+	// Math, with other vectors
+		Vector operator+(Vector oVec)
+		{
+			oVec.x += x, oVec.y += y, oVec.z += z;
+			return oVec;
+		}
+		Vector operator-(Vector oVec)
+		{
+			oVec.x -= x, oVec.y -= y, oVec.z -= z;
+			return oVec;
+		}
+		Vector operator*(Vector oVec)
+		{
+			oVec.x *= x, oVec.y *= y, oVec.z *= z;
+			return oVec;
+		}
+		Vector operator/(Vector oVec)
+		{
+			oVec.x /= x, oVec.y /= y, oVec.z /= z;
+			return oVec;
+		}
+	// Math, with ints/floats
+		Vector operator+(float i)
+		{
+			return operator+(Vector(i,i,i));
+		}
+		Vector operator-(float i)
+		{
+			return operator-(Vector(i,i,i));
+		}
+		Vector operator*(float i)
+		{
+			return operator*(Vector(i,i,i));
+		}
+		Vector operator/(float i)
+		{
+			return operator/(Vector(i,i,i));
+		}
+	// Comparison
+		bool operator==(Vector oVec)
+		{
+			return oVec.x == x && oVec.y == y && oVec.z == z;
+		}
+		bool operator!=(Vector oVec)
+		{
+			return !operator==(oVec);
+		}
+};
